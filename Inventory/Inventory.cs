95ef787bf17ee9b3f2ea997abc52ae42f0b50656
CLe4N <64629943+CLe4N@users.noreply.github.com>
2020-11-13 17:35:26 +0700

    Add files via upload

---
+++ Inventory/Inventory.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public bool[] isFull;
+    public GameObject[] slot;
+    public UseItem[] Item;
+
+    private void Update()
+    {
+        if (slot[0].transform.childCount > 1)
+        {
+            Item[0] = slot[0].transform.GetChild(1).GetComponent<UseItem>();
+        }
+        if (slot[1].transform.childCount > 1)
+        {
+            Item[1] = slot[1].transform.GetChild(1).GetComponent<UseItem>();
+        }
+
+        ItemKey();
+    }
+
+    void ItemKey()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (slot[0].transform.childCount > 1)
+            {
+                Item[0].CreateActiveItem();
+                Destroy(slot[0].transform.GetChild(1).gameObject);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (slot[1].transform.childCount > 1)
+            {
+                Item[1].CreateActiveItem();
+                Destroy(slot[1].transform.GetChild(1).gameObject);
+            }
+        }
+    }
+}
+
---
+++ Inventory/PickUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    Inventory inventory;
+    public GameObject itemButton;
+    void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            for (int i = 0; i < inventory.slot.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButton, inventory.slot[i].transform);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
